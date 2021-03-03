5a1799c2e20f6b25246313a96c22c9b617c533c2
Dylan Rafael <dylanrafael05@gmail.com>
2021-03-02 22:40:18 -0500

    Fixed fatal input management error
    - Removed all dependencies on Touch.fingerId remaining ordered over time
    - Changed the array of touchPoints to a dictionary
    - Added a method to deconstruct the dictionary to a NativeArray that contains only blittable structs, allocated using the "TempJob" allocator.

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcCore
+{
+    public static class Constants
+    {
+
+        public const float InputMaxY =  5.5f;
+        public const float InputMinX = -8.5f;
+        public const float InputMaxX =  8.5f;
+        public const float ArcYZero  =  1f;
+
+    }
+}
--- Assets/Scripts/MonoBehaviours/InputManager.cs
+++ Assets/Scripts/MonoBehaviours/InputManager.cs
@@ -1,49 +1,60 @@
 ﻿using ArcCore.Utility;
-using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
 namespace ArcCore.MonoBehaviours
 {
-    public enum TouchStatus
-    {
-        NONE,
-        PRESSED,
-        HELD
-    }
-
     public struct TouchPoint
     {
         public float2 inputPlanePoint;
         public int lane;
         public float time;
-        public TouchStatus status;
+        public bool pressed;
 
-        public TouchPoint(float2 inputPlanePoint, int lane, float time, TouchStatus status)
+        public TouchPoint(float2 inputPlanePoint, int lane, float time, bool pressed)
         {
             this.inputPlanePoint = inputPlanePoint;
-            this.lane  = lane;
+            this.lane = lane;
             this.time = time;
-            this.status = status;
+            this.pressed = pressed;
         }
-
-        public bool IsValid => status != TouchStatus.NONE;
     }
+
+    public struct TouchPointFull
+    {
+        public TouchPoint touchPoint;
+        public int fingerId;
+
+        public TouchPointFull(TouchPoint touchPoint, int fingerId)
+        {
+            this.touchPoint = touchPoint;
+            this.fingerId = fingerId;
+        }
+    }
+
+    //ORDERING IS IMPORTANT HERE; POLL_INPUT MUST OCCUR BEFORE ALL JUDGING.
+    //UNSURE HOW TO DO THIS
     public class InputManager : MonoBehaviour
     {
         public static InputManager Instance { get; private set; }
 
         public const int MaxTouches = 10;
 
-        public TouchPoint[] touchPoints = new TouchPoint[MaxTouches];
+        public Dictionary<int, TouchPoint> touchPoints = new Dictionary<int, TouchPoint>(MaxTouches);
 
         public Camera cameraCast;
         [HideInInspector]
         public float yLeniency;
+        public float BaseYLenDist { get; private set; }
 
         void Awake()
         {
             Instance = this;
+
+            //ENSURE THAT CAMERA IS RESET BEFORE CALLING THIS
+            BaseYLenDist = GetYLeniencyDist();
         }
 
         void Start()
@@ -51,51 +62,95 @@ namespace ArcCore.MonoBehaviours
             CreateConnection();
         }
 
+        public void CreateConnection()
+            => Conductor.Instance.OnTimeCalculated += PollInput;
+
         public void KillConnection()
             => Conductor.Instance.OnTimeCalculated -= PollInput;
 
-        public void CreateConnection()
-            => Conductor.Instance.OnTimeCalculated += PollInput;
+        public NativeArray<TouchPointFull> GetJobPreparedTouchPoints()
+        {
+            NativeArray<TouchPointFull> ret = new NativeArray<TouchPointFull>(touchPoints.Count, Allocator.TempJob);
+
+            int i = 0;
+
+            foreach(var touchPoint in touchPoints)
+                ret[i++] = new TouchPointFull(touchPoint.Value, touchPoint.Key);
+
+            return ret;
+        }
 
         private void PollInput(float time)
         {
             CalculateYLeniency(); //SHOULDNT BE RECALCULATED FOR CHARTS WITHOUT CAMERA MOTION
+
+            int count = 0;
             for(int i = 0; i < Input.touchCount; i++)
             {
                 Touch t = Input.touches[i];
 
-                if (t.fingerId > MaxTouches)
-                    continue;
+                if (t.phase == TouchPhase.Began && !touchPoints.ContainsKey(t.fingerId)) 
+                {
 
-                //todo: FIX ALL THE ISSUES WITH "t.fingerId - 1", FINGER-ID MIGHT BE ZERO INDEXED JASDHKJDHJKD
-                if (t.phase == TouchPhase.Began || t.phase == TouchPhase.Moved) 
-                {
                     (float2 ipt, int lane) = PerformRayCast(t);
-                    //UNSURE IF FINGER ID IS ZERO-INDEXED, TEST LATER
-                    //UNSURE IF DELTA-TIME AND TIME ARE OF MISMATCHED TIME UNITS, FRICK
-                    touchPoints[t.fingerId - 1] = new TouchPoint(ipt, lane, time - t.deltaTime, TouchStatus.PRESSED);
+
+                    if (touchPoints.Count <= MaxTouches)
+                        touchPoints.Add(
+                            t.fingerId, 
+                            new TouchPoint(ipt, lane, time - t.deltaTime, true)
+                        );
+
                 } 
-                else if (t.phase == TouchPhase.Stationary)
+                else if (t.phase == TouchPhase.Moved && touchPoints.ContainsKey(t.fingerId))
                 {
-                    touchPoints[t.fingerId - 1].status = TouchStatus.HELD;
-                    touchPoints[t.fingerId - 1].time = time - t.deltaTime;
+
+                    TouchPoint tp = touchPoints[t.fingerId];
+
+                    (float2 ipt, int lane) = PerformRayCast(t);
+
+                    tp.inputPlanePoint = ipt;
+                    tp.lane = lane;
+                    tp.pressed = false;
+
+                    touchPoints[t.fingerId] = tp;
+
+                    count++;
+
+                }
+                else if (t.phase == TouchPhase.Stationary && touchPoints.ContainsKey(t.fingerId))
+                {
+
+                    TouchPoint tp = touchPoints[t.fingerId];
+
+                    tp.pressed = false;
+                    tp.time = time - t.deltaTime;
+
+                    touchPoints[t.fingerId] = tp;
+
                 } 
                 else if (t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled)
                 {
-                    touchPoints[t.fingerId - 1].status = TouchStatus.NONE;
+
+                    touchPoints.Remove(t.fingerId);
+
                 }
             }
         }
 
-        public void CalculateYLeniency()
+        public float GetYLeniencyDist()
         {
             Vector3 basepoint = cameraCast.WorldToScreenPoint(Vector3.zero);
-            Vector3 toppoint = cameraCast.WorldToScreenPoint(Vector3.up * 4); //REPLACE 4 WITH HEIGHT OF INPUT AREA
-            float dist = Vector3.Distance(basepoint, toppoint);
+            Vector3 toppoint = cameraCast.WorldToScreenPoint(Vector3.up * Constants.InputMaxY);
+            return Vector3.Distance(basepoint, toppoint);
+        }
+
+        public void CalculateYLeniency()
+        {
+            float dist = GetYLeniencyDist();
             if (Mathf.Approximately(dist, 0))
                 yLeniency = float.PositiveInfinity;
             else
-                yLeniency = 1 / dist; // REPLACE 1 WITH THE NORMAL VALUE OF DIST; PROBABLY A CONST OR CALCULATED IN AWAKE BUT IM TOO LAZY LOL
+                yLeniency = BaseYLenDist / dist;
         }
 
         public (float2 ipt, int lane) PerformRayCast(Touch t)
@@ -116,7 +171,7 @@ namespace ArcCore.MonoBehaviours
             }
 
             int lane = 0;
-            if (ipt.y < 4 / 5 * yLeniency) //REPLACE 4 WITH THE NORMAL HEIGHT OF THE INPUT AREA
+            if (ipt.y < Constants.ArcYZero * yLeniency)
             {
                 ratio = ray.origin.y / ray.direction.y;
                 float laneX = ray.origin.x - ray.direction.x * ratio;
