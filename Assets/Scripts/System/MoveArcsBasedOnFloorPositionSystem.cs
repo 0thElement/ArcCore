31864bfde02f311293e91cb0be7f238bacbd91b5
0thElement <quangquangtrung89@gmail.com>
2021-03-04 17:30:27 +0700

    added a smol comment

--- Assets/Scripts/System/MoveArcsBasedOnFloorPositionSystem.cs
+++ Assets/Scripts/System/MoveArcsBasedOnFloorPositionSystem.cs
@@ -12,6 +12,7 @@ public class MoveArcBasedOnFloorPositionSystem : SystemBase
         float currentFloorPosition = Conductor.Instance.currentFloorPosition[0];
         Entities.ForEach((ref LocalToWorld lcwMatrix, in ArcStartPosition start, in FloorPosition floorPosition) => {
 
+            //the last column correspond to translation
             lcwMatrix.Value.c3.x = start.Value.x;
             lcwMatrix.Value.c3.y = start.Value.y;
             lcwMatrix.Value.c3.z = floorPosition.Value - currentFloorPosition;
