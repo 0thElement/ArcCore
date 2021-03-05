b9d15cf62dfab2dac8d67de3839f9eb9cd5175a2
0thElement <quangquangtrung89@gmail.com>
2021-03-05 14:02:12 +0700

    + Adding timinggroup component data + Simplified note movement system

---
+++ Assets/Scripts/System/MoveNotesTowardScreen.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MoveNotesTowardScreen : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float currentFloorPosition = Conductor.Instance.currentFloorPosition[0];
+        Entities.ForEach((ref Translation translation, in FloorPosition floorPosition) => {
+            translation.Value.z = floorPosition.Value - currentFloorPosition; 
+        }).Schedule();
+    }
+}
