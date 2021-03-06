e54339d16d0ef65e2c698b134c9ce6510901ccfa
Dylan Rafael <dylanrafael05@gmail.com>
2021-03-06 01:51:52 -0500

    Added fast projections onto xy, xz, yz planes, added leniency calc for x values

--- Assets/Scripts/Utility/ProjectionMaths.cs
+++ Assets/Scripts/Utility/ProjectionMaths.cs
@@ -3,10 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.Mathematics;
+using UnityEngine;
 
-namespace Assets.Scripts.Utility
+namespace ArcCore.Utility
 {
-    class ProjectionMaths
+    public static class ProjectionMaths
     {
+
+        public const float TAN_EPSILON = 0.1003f;
+
+        public static float2 ProjectOntoXYPlane(Vector3 position, Ray ray)
+        {
+            if (ray.direction.z == 0)
+            {
+                return new float2(ray.origin.x, 0);
+            }
+            else
+            {
+                float ratio = ray.origin.z / ray.direction.z;
+                return new float2(ray.origin.x - ray.direction.x * ratio, ray.origin.y - ray.direction.y * ratio);
+            }
+        }
+
+        public static float2 ProjectOntoXZPlane(Vector3 position, Ray ray)
+        {
+            if (ray.direction.y == 0)
+            {
+                return new float2(ray.origin.x, 0);
+            }
+            else
+            {
+                float ratio = ray.origin.y / ray.direction.y;
+                return new float2(ray.origin.x - ray.direction.x * ratio, ray.origin.z - ray.direction.z * ratio);
+            }
+        }
+
+        public static float2 ProjectOntoYZPlane(Vector3 position, Ray ray)
+        {
+            if (ray.direction.x == 0)
+            {
+                return new float2(ray.origin.y, 0);
+            }
+            else
+            {
+                float ratio = ray.origin.x / ray.direction.x;
+                return new float2(ray.origin.y - ray.direction.y * ratio, ray.origin.z - ray.direction.z * ratio);
+            }
+        }
+
+        public static (float min, float max) GetXExtents(float3 camPos, float2 projPos)
+        {
+            if (camPos.z == 0)
+                return (float.NegativeInfinity, float.PositiveInfinity);
+
+            float deltaY = camPos.y - projPos.y;
+            //dx is sqrt((camy - projy)^2 + cam z^2)
+            float distXProj = math.sqrt(deltaY * deltaY + camPos.z * camPos.z);
+
+            return (
+                //MINIMUM
+                distXProj * (projPos.x - camPos.x - TAN_EPSILON * distXProj) / (
+                distXProj + (projPos.x - camPos.x) * TAN_EPSILON),
+                //MAXIMUM
+                distXProj * (projPos.x - camPos.x + TAN_EPSILON * distXProj) / (
+                distXProj - (projPos.x - camPos.x) * TAN_EPSILON)
+            );
+        }
+
     }
 }
