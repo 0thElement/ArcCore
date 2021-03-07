3caaad849f17e61e2545fcd7a45b97ecb81fe101
Dylan Rafael <dylanrafael05@gmail.com>
2021-03-07 16:41:58 -0500

    Fixed possible time precision error due early to long -> float conversion

--- Assets/Scripts/Utility/Convert.cs
+++ Assets/Scripts/Utility/Convert.cs
@@ -28,7 +28,15 @@ namespace ArcCore.Utility
 
     public class TimeThreadless
     {
-        public static float SecondsExact => DateTime.Now.Ticks / 10_000_000f;
+        public static long Ticks => DateTime.Now.Ticks;
+        public static float Seconds => TicksToSec(Ticks);
+
+        public static float TicksToSec(long ticks) => ticks / 10_000_000f;
+
+        public static float TimeSince(float sec) => Seconds - sec;
+        public static long TimeSince(long ticks) => Ticks - ticks;
+
+        public static float TimeSince_T2S(long ticks) => TicksToSec(TimeSince(ticks));
     }
 
     public class Convert
