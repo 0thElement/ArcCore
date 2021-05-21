78d5892272dbdc951eaab50e209d2c240840b3fc
0thElement <quangquangtrung89@gmail.com>
2021-05-21 20:37:39 +0700

    Basic text particle implementation

--- Assets/Scripts/Behaviours/ParticleCreator.cs
+++ Assets/Scripts/Behaviours/ParticleCreator.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Mathematics;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ArcCore.Behaviours
@@ -8,37 +9,84 @@ namespace ArcCore.Behaviours
         public enum TextParticleType
         {
             LostJudgeType,
-            PureJudgeType,
-            FarJudgeType
+            FarJudgeType,
+            PureJudgeType
         }
 
         public static ParticleCreator Instance { get; private set; }
 
-        [SerializeField] private int tapJudgePoolSize;
-        [SerializeField] private int arcJudgePoolSize;
-        [SerializeField] private GameObject lostJudgeBase;
-        [SerializeField] private GameObject pureJudgeBase;
-        [SerializeField] private GameObject farJudgeBase;
-        [SerializeField] private GameObject tapEffectBase;
-        [SerializeField] private GameObject holdEffectBase;
+        [SerializeField] private int textParticlePoolSize;
+        [SerializeField] private int tapParticlePoolSize;
+        [SerializeField] private int arcParticlePoolSize;
+
+        [SerializeField] private Material[] textJudgeMaterials;
+
+        [SerializeField] private GameObject textParticleBase;
+        [SerializeField] private GameObject tapParticleBase;
+        [SerializeField] private GameObject arcParticleBase;
+        [SerializeField] private GameObject holdParticleBase;
+
         [SerializeField] private SpriteRenderer[] laneHighlights;
 
         private GameObject[] textParticlePool;
-        private GameObject[] spriteParticlePool;
+        private GameObject[] tapParticlePool;
         private GameObject[] arcParticlePool;
+
+        private int currentTextParticleIndex = 0;
+        private int currentTapParticleIndex = 0;
+        private int currentArcParticleIndex = 0;
+        private Dictionary<int, int> arcGroupToPoolIndex = new Dictionary<int, int>();
+        //TODO: SETUP HOLD PARTICLE
+        private void SetupPoolArray(ref GameObject[] array, int size, GameObject baseObject)
+        {
+            array = new GameObject[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = Instantiate(baseObject, transform);
+            }
+        }
+
+        private bool IncrementOrCycle(ref int toIncrement, int limit)
+        {
+            toIncrement++;
+            if (toIncrement == limit) 
+            {
+                toIncrement = 0;
+                return true;
+            }
+            return false;
+        }
 
         private void Awake()
         {
             Instance = this;
-            textParticlePool = new GameObject[tapJudgePoolSize];
-            spriteParticlePool = new GameObject[tapJudgePoolSize];
-            arcParticlePool = new GameObject[arcJudgePoolSize];
+
+            SetupPoolArray(ref textParticlePool, textParticlePoolSize, textParticleBase);
+            SetupPoolArray(ref tapParticlePool, tapParticlePoolSize, tapParticleBase);
+            SetupPoolArray(ref arcParticlePool, arcParticlePoolSize, arcParticleBase);
+
         }
 
+        //WILL REMOVE LATER
         public void PlayParticleAt(float2 position, TextParticleType type)
         {
-            
-            Debug.Log(position.x + " " + position.y + " " + type);
+            PlayTapParticleAt(position, type);
+        }
+
+        public void PlayTapParticleAt(float2 position, TextParticleType judgeType)
+        {
+            GameObject tap = tapParticlePool[currentTapParticleIndex];
+            GameObject text = textParticlePool[currentTextParticleIndex];
+
+            tap.GetComponent<Transform>().position = new float3(position, 0);
+            tap.GetComponent<ParticleSystem>().Play();
+
+            text.GetComponent<Transform>().position = new float3(position, 0);
+            text.GetComponent<Renderer>().material = textJudgeMaterials[(int)judgeType];
+            text.GetComponent<ParticleSystem>().Play();
+
+            IncrementOrCycle(ref currentTapParticleIndex, tapParticlePoolSize - 1);
+            IncrementOrCycle(ref currentTextParticleIndex, textParticlePoolSize - 1);
         }
     }
 }
